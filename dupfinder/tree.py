66f22ddf62380befed4d4bc5cb44a75edca1087e
Alex <alexpdev@github.com>
2020-04-29 02:07:37 -0700

    62562

--- dupfinder/tree.py
+++ dupfinder/tree.py
@@ -1,16 +1,23 @@
 import os,sys
 from pathlib import Path
-from PySide2.QtGui import Qt
-from PySide2.QtWidgets import QTableView,QFileSystemModel,QTreeView
+# from PySide2.QtGui import Qt
+from PySide2.QtCore import QItemSelectionModel
+
+from PySide2.QtWidgets import QTableView,QFileSystemModel,QTreeView,QDirModel,QAction
 
 
 class Tree(QTreeView):
-    def __init__(self,parent=None):
+    def __init__(self,parent=None,window=None):
         super().__init__(parent=parent)
         self.setObjectName("Tree View")
-        self.window = parent
+        self.window = window
         self.fsmodel = FileModel(parent=self)
         self.setModel(self.fsmodel)
+        self.fsmodel.hideColumns()
+        SM = self.selectionModel()
+
+
+
 
 
 
@@ -21,5 +28,8 @@ class FileModel(QFileSystemModel):
         self.setRootPath("C:")
         self.setResolveSymlinks(True)
         self.setReadOnly(False)
-        # self.setRootIndex(self.rootDirectory())
+
+    def hideColumns(self):
+        for i in range(1,self.columnCount()):
+            self.view.hideColumn(i)
 
